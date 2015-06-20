7e9782e20ef335d2fe9cc8df5eec9530b14759bc
Robert Krasowski <rkrasowski@yahoo.com>
2015-06-20 13:46:05 +0000

    websocket.pl works

---
+++ websocket.pl
@@ -0,0 +1,49 @@
+#!/usr/bin/env perl
+use Mojolicious::Lite;
+any '/' => 'index';
+websocket '/data' => sub {
+  my $self = shift;
+  my $timer = Mojo::IOLoop->recurring( 1 => sub {
+    state $i = 0;
+    $self->send({ json => gen_data($i++) });
+  });
+
+  $self->on( finish => sub {
+    Mojo::IOLoop->remove($timer);
+  });
+};
+
+sub gen_data {
+  my $x = shift;
+  return [ $x, sin( $x + 2*rand() - 2*rand() ) ]
+}
+
+app->start;
+
+__DATA__
+
+@@ index.html.ep
+
+% layout 'basic';
+
+%= javascript 'https://code.jquery.com/jquery-2.1.4.min.js';
+%= javascript 'https://cdnjs.cloudflare.com/ajax/libs/flot/0.8.3/jquery.flot.js'
+
+
+<div id="plot" style="width:600px;height:300px">
+</div>
+
+%= javascript begin
+  var data = [];
+  var plot = $.plot($('#plot'), [ data ]);
+
+  var url = '<%= url_for('data')->to_abs %>';
+  var ws = new WebSocket( url );
+  ws.onmessage = function(e){
+    var point = JSON.parse(e.data);
+    data.push(point);
+    plot.setData([data]);
+    plot.setupGrid();
+    plot.draw();
+  };
+% end
