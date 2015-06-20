bb4602eb458efe0d68124907345830850bb81edc
Robert Krasowski <rkrasowski@yahoo.com>
2015-06-20 16:05:30 +0000

    2 wartosci sie zmieniaja ale rownoczesnie

---
+++ 2test.pl
@@ -0,0 +1,87 @@
+#!/usr/bin/env perl
+use warnings;
+use strict;
+use Mojolicious::Lite;
+
+any '/' => 'index';
+
+websocket '/data' => sub {
+  	my $self = shift;
+	
+	# Websocked opens
+ 	$self->app->log->debug('WebSocket opened');
+
+
+
+#	 $self->on(message => sub {
+ #   my ($self, $msg) = @_;
+#	$msg = "Kurde mol\n";
+ #   $self->send("data: $msg");
+
+#}
+
+
+
+
+	 my $timer = Mojo::IOLoop->recurring( 1 => sub {
+ 	 state $i = 0;
+  	 $self->send({ json => gen_data($i++) });
+  });
+
+ 
+
+
+
+
+
+
+
+
+
+ 	$self->on( finish => sub {
+ #   	Mojo::IOLoop->remove($timer);
+	my ($self, $code, $reason) = @_;
+	$self->app->log->debug("WebSocket closed with status $code");
+  	});
+};
+
+
+sub gen_data 
+	{
+ 		my  $x = shift;
+  		return [ $x, sin( $x + 2*rand() - 2*rand() ) ]
+	}
+app->start;
+
+
+__DATA__
+
+@@ index.html.ep
+
+% layout 'basic';
+
+%= javascript 'https://code.jquery.com/jquery-2.1.4.min.js';
+
+
+
+<h1 id="TempIn">Testing the script</h1>
+<h1 id="TempOut">Temperature Out</h1>
+
+
+%= javascript begin
+  var data = [];
+
+  var url = '<%= url_for('data')->to_abs %>';
+  var ws = new WebSocket( url );
+  ws.onmessage = function(e){
+
+var tempIn = document.getElementById("TempIn");
+tempIn.innerHTML =  JSON.parse(e.data);
+
+var tempOut = document.getElementById("TempOut");
+tempOut.innerHTML =  JSON.parse(e.data);
+
+
+   
+  };
+% end
